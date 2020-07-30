8a653e46b2bf785671e5197b810170b1b6848518
Ézhor Malkávar <ezhormalkavar@gmail.com>
2020-07-30 20:40:43 +0200

    Real-time transmission seems to work. Offsets need to be corrected.

---
+++ Unity/Assets/Scripts/Adam.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Adam : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField]
+    private float delay;
+    [SerializeField]
+    private RotationDetector[] rotationDetectors;
+#pragma warning restore 0649
+
+    private readonly StringBuilder stringBuilder = new StringBuilder();
+
+    private void Start()
+    {
+        StartCoroutine(SendData());
+    }
+
+    private IEnumerator SendData()
+    {
+        UnityWebRequest request;
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FetchRotations();
+            request = UnityWebRequest.Post("http://192.168.0.31:2727", stringBuilder.ToString());
+            request.SendWebRequest();
+        }
+    }
+
+    private void FetchRotations()
+    {
+        stringBuilder.Clear();
+        foreach(RotationDetector rotationDetector in rotationDetectors)
+        {
+            stringBuilder.Append(rotationDetector.Rotation());
+        }
+    }
+}
